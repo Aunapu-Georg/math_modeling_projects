8930f8781daf4fbd8218323e8132ea4f6203b3b4
Aunapu-Georg <76224645+Aunapu-Georg@users.noreply.github.com>
2023-05-11 10:15:35 +0000

    Затестил плоскость

--- main.py
+++ main.py
@@ -10,23 +10,37 @@ N = 100
 alpha = np.linspace(10, 0, N)
 
 # Параметрическое задание пространственной кривой
-x = np.cos(alpha)
-y = np.sin(alpha)
-z = alpha * 0.1
+x_move = np.cos(alpha)
+y_move = np.sin(alpha)
+z_move = alpha * 0.1
 
 # Создание анимируемых объектов
-ball, = ax.plot(x, y, z, 'o', color='b') # Сферический объект
-line, = ax.plot(x, y, z, '-', color='b') # Линия
+ball, = ax.plot(x_move, y_move, z_move, 'o', color='b') # Сферический объект
+line, = ax.plot(x_move, y_move, z_move, '-', color='b') # Линия
 
-# Функция подстановки координат в анимируемые объекты
+# Функция подстановки координат в анимируемую траекторию и мяч
 def animate(i):
-    ball.set_data(x[i], y[i])
-    ball.set_3d_properties(z[i])
+    ball.set_data(x_move[i], y_move[i])
+    ball.set_3d_properties(z_move[i])
 
-    line.set_data(x[:i], y[:i])
-    line.set_3d_properties(z[:i])
+    line.set_data(x_move[:i], y_move[:i])
+    line.set_3d_properties(z_move[:i])
 
-# Украшательсвта и масштабирование
+# Анимирование траектории мяча и него самого
+ani = FuncAnimation(fig, animate, N, interval=50)
+
+# Определение параметров поверхности
+phi = np.linspace(10, 0, N)
+theta = alpha
+
+# Параметрическое задание поверхности
+x_surface = np.outer(phi, np.cos(theta))
+y_surface = np.outer(phi, np.sin(theta))
+z_surface = np.outer(np.ones(np.size(theta)), theta * 0.1)
+
+ax.plot_surface(x_surface, y_surface, z_surface)
+
+# Границы видимой зоны системы координат и названия осей
 ax.set_xlim3d([-1.0, 1.0])
 ax.set_xlabel('X')
 
@@ -36,18 +50,6 @@ ax.set_ylabel('Y')
 ax.set_zlim3d([-1.0, 1.0])
 ax.set_zlabel('Z')
 
-# Анимирование
-ani = FuncAnimation(fig, animate, N, interval=50)
-
-# Определение параметров поверхности
-phi = np.linspace(10, 0, N)
-theta = alpha
-
-# Параметрическое задание поверхности
-x = np.outer(phi, np.cos(theta))
-y = np.outer(phi, np.sin(theta))
-z = np.outer(np.ones(np.size(theta)), theta * 0.1)
-
-ax.plot_surface(x, y, z)
+# Сохранение итоговой гифки
 
 ani.save('ani.gif')
