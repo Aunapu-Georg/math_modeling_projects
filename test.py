51430d5779a9a33830a7d2b3b2f63e58dd41faa7
Aunapu-Georg <76224645+Aunapu-Georg@users.noreply.github.com>
2023-05-12 19:50:24 +0000

    Попытался реализовать вращение около своей оси

--- test.py
+++ test.py
@@ -1,19 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import imageio
+import os
+ 
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ 
+N = 20
+edge = 20
+step = 5
+phi = np.linspace(0, 2*np.pi, 100)
+theta = np.linspace(0, np.pi, 100)
+ 
+def animate(delta):
+    x = np.outer(phi, np.cos(theta)) + np.outer(np.ones(np.size(theta)) * delta, np.ones(np.size(theta)))
+    y = np.outer(phi, np.sin(theta))
+    z = np.outer(np.ones(np.size(theta)), theta * step)
+    return x, y, z
+ 
+for i in range(N):
+ 
+    coordinates = animate(0.5 * i)
+    x = coordinates[0]
+    y = coordinates[1]
+    z = coordinates[2]
+    ax.plot_surface(x, y, z, color='b')
+    plt.savefig(f'pic_{i}')
 
-# Определение параметров поверхности
-phi = np.linspace(0, 2 * np.pi, 100)
-theta = np.linspace(0, 2 * np.pi, 100)
+ax.set_xlim3d([-edge, edge])
+ax.set_xlabel('X')
 
-# Параметрическое задание поверхности
+ax.set_ylim3d([-edge, edge])
+ax.set_ylabel('Y')
 
-# Параметрическое задание поверхности
-x_surface = np.outer(phi, np.cos(theta))
-y_surface = np.outer(phi, np.sin(theta))
-z_surface = np.outer(np.ones(np.size(theta)), theta * 0.1)
+ax.set_zlim3d([-edge, edge])
+ax.set_zlabel('Z')
 
-ax.plot_surface(x_surface, y_surface, z_surface)
-
-plt.savefig('test.png')
+# Создание анимации из отдельных кадров
+images = []
+filenames = [f'pic_{i}.png' for i in range(N)]
+for filename in filenames:
+    images.append(imageio.imread(filename))
+    os.remove(filename)
+imageio.mimsave('test.gif', images)
