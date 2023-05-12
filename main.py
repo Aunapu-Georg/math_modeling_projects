c27e8bb7715a1baa030debc6588be4c6bc6a064e
Aunapu-Georg <76224645+Aunapu-Georg@users.noreply.github.com>
2023-05-12 12:36:21 +0000

    Довел до ума основную часть

--- main.py
+++ main.py
@@ -5,14 +5,23 @@ import numpy as np
 # Создание пространства для анимации
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-# Определение параметров пространственной кривой
+# Определение параметров пространственной кривой и геликоида
 N = 100
-step = 0.1
-alpha = np.linspace(2 * np.pi, 0, N)
+step = 3
+extension = 2 # Прямопропорциональна радиусу спирали, по которой движется мяч
+size = 2 * np.pi
+
+width = size / 2
+height = size
+
+alpha = np.linspace(size, 0, N) # Длина траектории мяча
+
+phi = np.linspace(0, width, N) # "Ширина" поверхности
+theta = np.linspace(0, height, N) # "Высота" поверхности
 
 # Параметрическое задание пространственной кривой
-x_move = np.cos(alpha)
-y_move = np.sin(alpha)
+x_move = extension * np.cos(alpha)
+y_move = extension * np.sin(alpha)
 z_move = alpha * step
 
 # Создание анимируемых объектов
@@ -27,10 +36,6 @@ def animate(i):
     line.set_data(x_move[:i], y_move[:i])
     line.set_3d_properties(z_move[:i])
 
-# Определение параметров поверхности
-phi = np.linspace(0, 2 * np.pi, N)
-theta = np.linspace(0, 2 * np.pi, N)
-
 # Параметрическое задание поверхности
 x_surface = np.outer(phi, np.cos(theta))
 y_surface = np.outer(phi, np.sin(theta))
@@ -39,20 +44,20 @@ z_surface = np.outer(np.ones(np.size(theta)), theta * step)
 ax.plot_surface(x_surface, y_surface, z_surface)
 
 # Границы видимой зоны системы координат и названия осей
-"""
-ax.set_xlim3d([-1.0, 1.0])
+
+ax.set_xlim3d([-width, width])
 ax.set_xlabel('X')
 
-ax.set_ylim3d([-1.0, 1.0])
+ax.set_ylim3d([-width, width])
 ax.set_ylabel('Y')
 
-ax.set_zlim3d([-1.0, 1.0])
+ax.set_zlim3d([0, height * step])
 ax.set_zlabel('Z')
-"""
+
 # Подстановка координат для анимации
 
 ani = FuncAnimation(fig, animate, N, interval=50)
 
-# Сохранение итоговой гифки
+# Сохранение готовой анимации
 
 ani.save('ani.gif')
