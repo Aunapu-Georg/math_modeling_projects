2c90e8afce7872ef67a66a0b8dd176872f309791
Aunapu-Georg <76224645+Aunapu-Georg@users.noreply.github.com>
2023-05-13 08:33:59 +0000

    Протестировал конфигурации,  осевой поворот

--- test.py
+++ test.py
@@ -1,44 +1,48 @@
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
-import matplotlib.pyplot as plt
-import imageio
-import os
- 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
- 
-N = 20
-edge = 20
-step = 5
-phi = np.linspace(0, 2*np.pi, 100)
-theta = np.linspace(0, np.pi, 100)
- 
-def animate(delta):
-    x = np.outer(phi, np.cos(theta)) + np.outer(np.ones(np.size(theta)) * delta, np.ones(np.size(theta)))
-    y = np.outer(phi, np.sin(theta))
-    z = np.outer(np.ones(np.size(theta)), theta * step)
-    return x, y, z
- 
-for i in range(N):
- 
-    coordinates = animate(0.5 * i)
-    x = coordinates[0]
-    y = coordinates[1]
-    z = coordinates[2]
-    ax.plot_surface(x, y, z, color='b')
-    plt.savefig(f'pic_{i}')
 
-ax.set_xlim3d([-edge, edge])
+# Создание пространства для анимации
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Определение параметров пространственной кривой и геликоида
+N = 100 # Количество шагов для расчёта параметров (больше = точнее)
+step = 5 # Регулирует высоту всей поверхности (прямопропорционально)
+
+alpha = np.linspace(2 * np.pi, 0, N) # Длина траектории мяча
+
+# Параметрическое задание пространственной кривой
+x_move = np.cos(alpha) + np.sin(alpha)
+y_move = np.sin(alpha) + np.cos(alpha)
+z_move = alpha * step
+
+# Создание анимируемых объектов
+ball, = ax.plot(x_move, y_move, z_move, 'o', color='b') # Мяч
+line, = ax.plot(x_move, y_move, z_move, '-', color='b') # Траектория
+
+# Функция подстановки координат в анимируемую траекторию и мяч
+def animate(i):
+    ball.set_data(x_move[i], y_move[i])
+    ball.set_3d_properties(z_move[i])
+
+    line.set_data(x_move[:i], y_move[:i])
+    line.set_3d_properties(z_move[:i])
+
+# Границы видимой зоны системы координат и названия осей
+"""
+ax.set_xlim3d([np.pi, 2 * np.pi])
 ax.set_xlabel('X')
 
-ax.set_ylim3d([-edge, edge])
+ax.set_ylim3d([-2 * np.pi, 2 * np.pi])
 ax.set_ylabel('Y')
 
-ax.set_zlim3d([-edge, edge])
+ax.set_zlim3d([0, 2 * np.pi * step])
 ax.set_zlabel('Z')
+"""
+# Подстановка координат для анимации
 
-# Создание анимации из отдельных кадров
-images = []
-filenames = [f'pic_{i}.png' for i in range(N)]
-for filename in filenames:
-    images.append(imageio.imread(filename))
-    os.remove(filename)
-imageio.mimsave('test.gif', images)
+ani = FuncAnimation(fig, animate, N, interval=50)
+
+# Сохранение готовой анимации
+
+ani.save('test.gif')
